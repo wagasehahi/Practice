8708546af8a9deb299a37a874f67531fa6241e2e
hahi77 <166590795+wagasehahi@users.noreply.github.com>
2024-07-04 11:48:52 +0900

    Create MaxOfThree.cpp

---
+++ MaxOfThree/MaxOfThree.cpp
@@ -0,0 +1,31 @@
+//==========================================================
+//  3値の最大値
+//==========================================================
+#include <stdio.h>
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+//  関数プロトタイプ
+int MaxOfThree(int a, int b, int c);
+
+int main()
+{
+    srand(time(NULL));
+    int a = rand() % 100;
+    int b = rand() % 100;
+    int c = rand() % 100;
+    int ans = MaxOfThree(a, b, c);
+    printf("MaxOfThree(%d, %d, %d): %d\n", a, b, c, ans);
+    return 0;
+}
+
+int MaxOfThree(int a, int b, int c)
+{
+    int max = a;
+    if (max < b) {
+        max = b;
+    }
+    if (max < c) {
+        max = c;
+    }
+    return max;
+}
